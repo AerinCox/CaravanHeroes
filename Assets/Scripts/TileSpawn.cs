3638209dbcd55da990eb7446db29351cce274e95
Steven Kraft <kraftydinosaur@gmail.com>
2018-01-22 17:52:04 -0500

    Cleaned/Simplified TileSpawn Code

--- Assets/Scripts/TileSpawn.cs
+++ Assets/Scripts/TileSpawn.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TileSpawn : MonoBehaviour {
 	
 	Tile[,] map;
+	int rectX, rectY;
 
 	public Tile getTile(Point point){
 		if(point.x >= map.GetLength(0) || point.x < 0 || point.y >= map.GetLength(1) || point.y < 0){
@@ -16,6 +17,84 @@ public class TileSpawn : MonoBehaviour {
 	
 	public Tile[,] getMap(){
 		return this.map;
+	}
+	
+	public Point[] getPoints(int x1, int x2, int y1, int y2){
+		Point rect1 = new Point(Random.Range(x1,x2), Random.Range(y1,y2));
+		Point rect2 = new Point(Random.Range(x1,x2), Random.Range(y1,y2));
+		// 1 should always be higher than 2
+		if(rect1.x < rect2.x){
+			return new Point[] {rect2, rect1};
+		}
+		return new Point[] {rect1, rect2};
+	}
+	
+	public void connect(Point[] points1, int num1, Point[] points2, int num2, int option){
+		int breaker = 0;
+		int x = points1[num1].x;
+		int y = points1[num1].y;
+		if(points1[num1].x == points2[num2].x && points1[num1].y == points2[num2].y){
+			if(!map[x,y].filled){
+				map[x,y] = new Tile(new Vector3(x,0,y));
+			}
+		}
+		else{
+			while((x != points2[num2].x || y != points2[num2].y) && breaker != 15) {
+				if(!map[x,y].filled){
+					map[x,y] = new Tile(new Vector3(x,0,y));
+				}
+				
+				if (option == 0){
+					if(points2[num2].x > x){x++;}
+					if(points2[num2].y > y){y++;}
+				}
+				else if (option == 1){
+					if(points1[num1].x > points2[num2].x){
+						if(points2[num2].x > x){x--;}
+					}
+					else if(points2[num2].x > x){x++;}
+					if(points2[num2].y > y){y++;}
+				}
+				else if (option == 2){
+					if(points2[num2].x < x){x--;}
+					if(points2[num2].y > y){y++;}
+				}
+				else if (option == 3){
+					if(points2[num2].x < x){x--;}
+					if(points1[num1].y > points2[num2].y){
+						if(points2[num2].y < y){y--;}
+					}
+					else if(points2[num2].y > y){y++;}
+				}
+				else if (option == 4){
+					if(points2[num2].x < x){x--;}
+					if(points2[num2].y < y){y--;}
+				}
+				else if (option == 5){
+					if(points1[num1].x > points2[num2].x){
+						if(points2[num2].x < x){x--;}
+					}
+					else if(points2[num2].x < x){x++;}
+					if(points2[num2].y < y){y--;}
+				}
+				else if (option == 6){
+					if(points2[num2].x > x){x++;}
+					if(points2[num2].y < y){y--;}
+				}
+				else if (option == 7){
+					if(points2[num2].x > x){x++;}
+					if(points2[num2].y > points1[num1].y){
+						if(points2[num2].y > y){y++;}
+					}
+					else if(points2[num2].y < y){y--;}
+				}
+				
+				breaker++;
+			}
+			if(!map[x,y].filled){
+				map[x,y] = new Tile(new Vector3(x,0,y));
+			}
+		}
 	}
 
 	void Start () {
@@ -31,321 +110,42 @@ public class TileSpawn : MonoBehaviour {
 		
 		// Using idea from http://www.roguebasin.com/index.php?title=Irregular_Shaped_Rooms to generate irregular shaped rooms
 		//Getting left rectangle points
-		Point tempPoint;
-		int rectX = Random.Range(1,4);
-		int rectY = Random.Range(0,2);
-		Point leftRect1 = new Point(rectX, rectY);
-		rectX = Random.Range(1,4);
-		rectY = Random.Range(0,2);
-		Point leftRect2 = new Point(rectX, rectY);
-		// 1 should always be higher than 2
-		if(leftRect1.x < leftRect2.x){
-			tempPoint = leftRect2;
-			leftRect2 = leftRect1;
-			leftRect1 = tempPoint;
-		}
-		//Getting Upper rectangle points
-		rectX = Random.Range(3,5);
-		rectY = Random.Range(1,4);
-		Point upperRect1 = new Point(rectX, rectY);
-		rectX = Random.Range(3,5);
-		rectY = Random.Range(1,4);
-		Point upperRect2 = new Point(rectX, rectY);
-		if(upperRect1.y > upperRect2.y){
-			tempPoint = upperRect2;
-			upperRect2 = upperRect1;
-			upperRect1 = tempPoint;
-		}
-		//Getting Right rectangle points
-		rectX = Random.Range(1,4);
-		rectY = Random.Range(3,5);
-		Point rightRect1 = new Point(rectX, rectY);
-		rectX = Random.Range(1,4);
-		rectY = Random.Range(3,5);
-		Point rightRect2 = new Point(rectX, rectY);
-		if(rightRect1.x < rightRect2.x){
-			tempPoint = rightRect2;
-			rightRect2 = rightRect1;
-			rightRect1 = tempPoint;
-		}
-		//Getting Lower rectangle points
-		rectX = Random.Range(0,2);
-		rectY = Random.Range(1,4);
-		Point lowerRect1 = new Point(rectX, rectY);
-		rectX = Random.Range(0,2);
-		rectY = Random.Range(1,4);
-		Point lowerRect2 = new Point(rectX, rectY);
-		if(lowerRect1.y < lowerRect2.y){
-			tempPoint = lowerRect2;
-			lowerRect2 = lowerRect1;
-			lowerRect1 = tempPoint;
-		}
+		
+		Point[] leftRects = getPoints(1,4,0,2);
+		Point[] upperRects = getPoints(3,5,1,4);
+		Point[] rightRects = getPoints(1,4,3,5);
+		Point[] lowerRects = getPoints(0,2,1,4);
 
 		//Line Connecting Code
-		//Connecting left to up
-		int breaker = 0;
-		int x = leftRect1.x;
-		int y = leftRect1.y;
-		if(leftRect1.x == upperRect1.x && leftRect1.y == upperRect1.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != upperRect1.x || y != upperRect1.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(upperRect1.x > x){
-					x++;
-				}
-				if(upperRect1.y > y){
-					y++;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting up1 to up2
-		breaker = 0;
-		x = upperRect1.x;
-		y = upperRect1.y;
-		if(upperRect1.x == upperRect2.x && upperRect1.y == upperRect2.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != upperRect2.x || y != upperRect2.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(upperRect1.x > upperRect2.x){
-					if(upperRect2.x > x){
-						x--;
-					}
-				}
-				else{
-					if(upperRect2.x > x){
-						x++;
-					}
-				}
-				if(upperRect2.y > y){
-					y++;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting upper to right
-		breaker = 0;
-		x = upperRect2.x;
-		y = upperRect2.y;
-		if(upperRect2.x == rightRect1.x && upperRect2.y == rightRect1.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != rightRect1.x || y != rightRect1.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(rightRect1.x < x){
-					x--;
-				}
-				if(rightRect1.y > y){
-					y++;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting right1 to right2
-		breaker = 0;
-		x = rightRect1.x;
-		y = rightRect1.y;
-		if(rightRect1.x == rightRect2.x && rightRect2.y == rightRect1.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != rightRect2.x || y != rightRect2.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(rightRect2.x < x){
-					x--;
-				}
-				if(rightRect1.y > rightRect2.y){
-					if(rightRect2.y < y){
-						y--;
-					}
-				}
-				else{
-					if(rightRect2.y > y){
-						y++;
-					}
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting right2 to lower
-		breaker = 0;
-		x = rightRect2.x;
-		y = rightRect2.y;
-		if(rightRect2.x == lowerRect1.x && rightRect2.y == lowerRect1.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != lowerRect1.x || y != lowerRect1.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(lowerRect1.x < x){
-					x--;
-				}
-				if(lowerRect1.y < y){
-					y--;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting lower to lower
-		breaker = 0;
-		x = lowerRect1.x;
-		y = lowerRect1.y;
-		if(lowerRect1.x == lowerRect2.x && lowerRect1.y == lowerRect2.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != lowerRect2.x || y != lowerRect2.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(lowerRect1.x > lowerRect2.x){
-					if(lowerRect2.x < x){
-						x--;
-					}
-				}
-				else{
-					if(lowerRect2.x < x){
-						x++;
-					}
-				}
-				if(lowerRect2.y < y){
-					y--;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting lower to left
-		breaker = 0;
-		x = lowerRect2.x;
-		y = lowerRect2.y;
-		if(lowerRect2.x == leftRect2.x && lowerRect2.y == leftRect2.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != leftRect2.x || y != leftRect2.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(leftRect2.x > x){
-					x++;
-				}
-				if(leftRect2.y < y){
-					y--;
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		//Connecting left to left
-		breaker = 0;
-		x = leftRect2.x;
-		y = leftRect2.y;
-		if(leftRect2.x == leftRect1.x && leftRect1.y == leftRect2.y){
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
-		else{
-			while((x != leftRect1.x || y != leftRect1.y) && breaker != 15) {
-				if(map[x,y].filled == false){
-					map[x,y] = new Tile(new Vector3(x,0,y));
-				}
-				if(leftRect1.x > x){
-					x++;
-				}
-				if(leftRect1.y > leftRect2.y){
-					if(leftRect1.y > y){
-						y++;
-					}
-				}
-				else{
-					if(leftRect1.y < y){
-						y--;
-					}
-				}
-				breaker++;
-			}
-			if(map[x,y].filled == false){
-				map[x,y] = new Tile(new Vector3(x,0,y));
-			}
-		}
+		
+		connect(leftRects, 0, upperRects, 0, 0); //Connecting left to up
+		connect(upperRects, 0, upperRects, 1, 1); //Connecting up1 to up2
+		connect(upperRects, 1, rightRects, 0, 2); //Connecting upper to right	
+		connect(rightRects, 0, rightRects, 1, 3); //Connecting right1 to right2
+		connect(rightRects, 1, lowerRects, 0, 4); //Connecting right2 to lower
+		connect(lowerRects, 0, lowerRects, 1, 5); //Connecting lower to lower
+		connect(lowerRects, 1, leftRects, 1, 6); //Connecting lower to left
+		connect(leftRects, 1, leftRects, 0, 7); //Connecting left to left
 		
 		//Filling in the center of map
-		bool filled = false;
+		bool filled;
 		for(int i = 0; i < 5; i++){
-			for(int j = 0; j < 5; j++){
-				if(map[i,j].filled == true && filled == true){
-					filled = false;
+			filled = false;
+			for(int j = 0; j < 5; j++){		
+				if (map[i,j].filled) {
+					filled = !filled;
 				}
-				else if(map[i,j].filled == true){
-					filled = true;
-					continue;
-				}
-				if(filled == true && map[i,j].filled != true){
+				else if (filled) {
 					map[i,j] = new Tile(new Vector3(i,0,j));
 				}
 			}
-			filled = false;
 		}
 		
 		//Making sure player spawn area is filled... 
-		if(map[1,1].filled == false){
-			map[1,1] = new Tile(new Vector3(1,0,1));
-		}
-		if(map[2,1].filled == false){
-			map[2,1] = new Tile(new Vector3(2,0,1));
-		}
-		if(map[3,1].filled == false){
-			map[3,1] = new Tile(new Vector3(3,0,1));
+		for(int i=1; i <= 3; i++){
+			if(!map[i,1].filled){
+				map[i,1] = new Tile(new Vector3(i,0,1));
+			}
 		}
 	}
 }
